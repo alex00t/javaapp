25ef0bd99c9a7870816fd159a70d2c2cdbf454e5
Mohammad Ibrahim <alex00tipu@gmail.com>
2024-08-19 21:55:39 +0530

    Initial commit

---
+++ src/main/java/javaapp/Test.java
@@ -0,0 +1,8 @@
+package javaapp;
+
+public class Test {
+	public static void main(String [] args) {
+		System.out.println("hello this is my first program in git hub");
+	}
+
+}
---
+++ target/javaapp/index.jsp
@@ -0,0 +1,7 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<p>this is index page</p>
+<a href="about.jsp">about page</a>
+</body>
+</html>
